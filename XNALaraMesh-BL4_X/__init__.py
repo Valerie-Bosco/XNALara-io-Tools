402759521c3d9012238ca43889f04e151b0d6843
Valery-AA <valeryatart@gmail.com>
2024-11-27 11:35:37 +0100

    v1.1.0a

--- XNALaraMesh-BL4_X/__init__.py
+++ XNALaraMesh-BL4_X/__init__.py
@@ -1,3 +1,12 @@
+import importlib
+from .Alx_module_manager import (
+    Alx_Module_Manager
+)
+
+
+from . import xps_tools
+from . import addon_updater_ops
+
 bl_info = {
     "name": "XNALara/XPS Import/Export",
     "author": "Valeria Bosco[Valy Arhal], johnzero7[Original Developer]",
@@ -9,94 +18,23 @@ bl_info = {
     "category": "Import-Export",
 }
 
-#region ModuleAutoloader
-import importlib
-from os import sep as os_separator
-from pathlib import Path
-
-import bpy
-
-from . import addon_updater_ops
-
-
-folder_name_blacklist: list[str]=["__pycache__"] 
-file_name_blacklist: list[str]=["__init__.py"]
-file_name_blacklist.extend(["addon_updater", "addon_updater_ops"])
-
-
-addon_folders = []
-addon_files = []
-
-addon_path_iter = [ Path( __path__[0] ) ]
-addon_path_iter.extend(Path( __path__[0] ).iterdir())
-
-for folder_path in addon_path_iter:
-    
-    
-    if ( folder_path.is_dir() ) and ( folder_path.exists() ) and ( folder_path.name not in folder_name_blacklist ):
-        addon_folders.append( folder_path )
-
-        for subfolder_path in folder_path.iterdir():
-            if ( subfolder_path.is_dir() ) and ( subfolder_path.exists()):
-                addon_path_iter.append( subfolder_path )
-                addon_folders.append( subfolder_path )
-
-addon_files = [[folder_path, file_name.name[0:-3]] for folder_path in addon_folders for file_name in folder_path.iterdir() if ( file_name.is_file() ) and ( file_name.name not in file_name_blacklist ) and ( file_name.suffix == ".py" )]
-
-for folder_file_batch in addon_files:
-    file = folder_file_batch[1]
-    
-    if (file not in locals()):
-        relative_path = str(folder_file_batch[0].relative_to( __path__[0] ) ).replace(os_separator,"." )
-
-        import_line = f"from . {relative_path if relative_path != '.' else ''} import {file}"
-        exec(import_line)
-    else:
-        reload_line = f"{file} = importlib.reload({file})"
-        exec(reload_line)
-
-import inspect
-alx_class_object_list = tuple(alx_class[1] for file_batch in addon_files for alx_class in inspect.getmembers(eval(file_batch[1]), inspect.isclass) )
-
-AlxClassQueue = alx_class_object_list
-#endregion
-
-
-from . import xps_tools
-
-
-def AlxRegisterClassQueue():
-    for AlxClass in AlxClassQueue:
-        try:
-            bpy.utils.register_class(AlxClass)
-        except Exception as error:
-            print(error)
-            try:
-                bpy.utils.unregister_class(AlxClass)
-                bpy.utils.register_class(AlxClass)
-            except Exception as error:
-                print(error)
-
-def AlxUnregisterClassQueue():
-    for AlxClass in AlxClassQueue:
-        try:
-            bpy.utils.unregister_class(AlxClass)
-        except:
-            print("Can't Unregister", AlxClass)
+module_loader = Alx_Module_Manager(__path__, globals())
 
 
 def register():
     addon_updater_ops.update_path_fix = __path__
     addon_updater_ops.register(bl_info)
 
-    AlxRegisterClassQueue()
+    module_loader.developer_register_modules()
     xps_tools.register()
+
 
 def unregister():
     addon_updater_ops.unregister()
 
-    AlxUnregisterClassQueue()
+    module_loader.developer_unregister_modules()
     xps_tools.unregister()
-    
+
+
 if __name__ == "__main__":
     register()
