105b2e63fdfd199c9ae5fab9878b6a0e89ecced9
Valery-AA <valeryatart@gmail.com>
2025-01-28 19:15:24 +0100

    missing preference fix

--- XNALara-io-Tools/xnal_preferences.py
+++ XNALara-io-Tools/xnal_preferences.py
@@ -1,6 +1,8 @@
 import bpy
 
-from . addon_updater_ops import make_annotations, update_settings_ui
+from .modules.addon_updater_system.addon_updater import (make_annotations,
+                                                         update_settings_ui)
+
 
 @make_annotations
 class XNAlaraMesh4X_AddonPreferences(bpy.types.AddonPreferences):
@@ -8,13 +10,12 @@ class XNAlaraMesh4X_AddonPreferences(bpy.types.AddonPreferences):
 
     bl_idname = __package__
 
-    auto_check_update : bpy.props.BoolProperty(name="Auto-check for Update", description="If enabled, auto-check for updates using an interval", default=False) #type:ignore
+    auto_check_update: bpy.props.BoolProperty(name="Auto-check for Update", description="If enabled, auto-check for updates using an interval", default=False)  # type:ignore
 
-    updater_interval_months : bpy.props.IntProperty(name='Months', description="Number of months between checking for updates", default=0, min=0) #type:ignore
-    updater_interval_days : bpy.props.IntProperty(name='Days', description="Number of days between checking for updates", default=7, min=0, max=31) #type:ignore
-    updater_interval_hours : bpy.props.IntProperty(name='Hours', description="Number of hours between checking for updates", default=0, min=0, max=23) #type:ignore
-    updater_interval_minutes : bpy.props.IntProperty(name='Minutes', description="Number of minutes between checking for updates", default=0, min=0, max=59) #type:ignore
-
+    updater_interval_months: bpy.props.IntProperty(name='Months', description="Number of months between checking for updates", default=0, min=0)  # type:ignore
+    updater_interval_days: bpy.props.IntProperty(name='Days', description="Number of days between checking for updates", default=7, min=0, max=31)  # type:ignore
+    updater_interval_hours: bpy.props.IntProperty(name='Hours', description="Number of hours between checking for updates", default=0, min=0, max=23)  # type:ignore
+    updater_interval_minutes: bpy.props.IntProperty(name='Minutes', description="Number of minutes between checking for updates", default=0, min=0, max=59)  # type:ignore
 
     def draw(self, context):
         update_settings_ui(self, context)
