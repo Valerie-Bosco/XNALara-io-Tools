4887063168ab8010e642a092353ee5b4cf512365
Valery-AA <valeryatart@gmail.com>
2025-03-01 07:05:16 +0100

    version bump

--- XNALara-io-Tools/__init__.py
+++ XNALara-io-Tools/__init__.py
@@ -8,7 +8,7 @@ bl_info = {
     "name": "XNALara-io-Tools",
     "author": "Valerie Bosco[Valy Arhal], johnzero7[Original Developer]",
     "description": "Import-Export XNALara/XPS files",
-    "version": (1, 1, 1),
+    "version": (1, 1, 2),
     "blender": (4, 0, 0),
     "category": "Import-Export",
     "location": "File > Import-Export > XNALara/XPS",
