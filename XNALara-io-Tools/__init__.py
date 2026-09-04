69a515164803fc82c7a0aedcf3ae4e511478e596
Valerie-Bosco <valeriearhal.business@gmail.com>
2026-09-04 21:08:46 +0200

    fix: version bump -release

--- XNALara-io-Tools/__init__.py
+++ XNALara-io-Tools/__init__.py
@@ -9,7 +9,7 @@ bl_info = {
     "name": "XNALara-io-Tools",
     "author": "Valerie Bosco[Valy Arhal], johnzero7[Original Developer]",
     "description": "Import-Export for XNALara/XPS files",
-    "version": (1, 3, 8),
+    "version": (1, 3, 9),
     "blender": (3, 6, 0),
     "category": "Import-Export",
     "location": "File > Import-Export > XNALara/XPS",
